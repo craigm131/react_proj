29c2f21ca598dfd6f8fbd84ec6d3e591b46b3136
Craig Millis <craig.millis@zayo.com>
2019-07-24 16:38:27 -0600

    tut 3

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLeads } from '../../actions/leads';
 
-class Leads extends Component {
+export class Leads extends Component {
+    static propTypes = {
+        leads: PropTypes.array.isRequired
+    }
+
+    componentDidMount() {
+        this.props.getLeads();
+    }
+
     render() {
         return (
             <div>
@@ -11,4 +22,8 @@ class Leads extends Component {
     }
 }
 
-export default Leads;
+const mapStateToProps = state => ({
+    leads: state.leads.leads
+});
+
+export default connect(mapStateToProps, { getLeads })(Leads);
