2ca1581ecae82aa68ed7e2928d1fa0a65dbd58af
Craig Millis <craig.millis@zayo.com>
2019-09-09 16:49:36 -0600

    leads from submits and deletes

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,6 +1,6 @@
-import { combineReducers} from "redux";
+import { combineReducers } from "redux";
 import leads from './leads';
-
+//pass in an object of any reducers that we have, like leads reducer
 export default combineReducers({
     leads
-})
+});
--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,15 +1,25 @@
-import { GET_LEADS } from '../actions/types.js';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from '../actions/types.js';
 
 const initialState={
     leads: []
 }
 
 export default function(state = initialState, action) {
-    switch(action.type){
-        case GET_LEADS:
+    switch (action.type) {
+        case GET_LEADS:  //called from components/leads/Leads.js
+            return {
+                ...state,  //this will return whatever else is in the state in addition to leads
+                leads: action.payload
+            };
+        case DELETE_LEAD:  //called from components/leads/Leads.js
             return {
                 ...state,
-                leads: action.payload
+                leads: state.leads.filter(lead => lead.id !== action.payload)
+            };
+        case ADD_LEAD:  //called from components/leads/Leads.js
+            return {
+                ...state,
+                leads: [...state.leads, action.payload]
             };
         default:
             return state;
